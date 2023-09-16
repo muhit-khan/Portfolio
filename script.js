fe520fe6ff04a707f029fac26ad8156156f4b7e8
Muhit Khan <muhitkhan_bd@outlook.com>
2023-09-16 11:36:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,75 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
+$(document).ready(function () {
+    $(window).scroll(function () {
         // sticky navbar on scroll script
-        if(this.scrollY > 20){
+        if (this.scrollY > 20) {
             $('.navbar').addClass("sticky");
-        }else{
+        } else {
             $('.navbar').removeClass("sticky");
         }
-        
+
         // scroll-up button show/hide script
-        if(this.scrollY > 500){
+        if (this.scrollY > 500) {
             $('.scroll-up-btn').addClass("show");
-        }else{
+        } else {
             $('.scroll-up-btn').removeClass("show");
         }
     });
 
     // slide-up script
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
+    $('.scroll-up-btn').click(function () {
+        $('html').animate({ scrollTop: 0 });
         // removing smooth scroll on slide-up button click
         $('html').css("scrollBehavior", "auto");
     });
 
-    $('.navbar .menu li a').click(function(){
+    $('.navbar .menu li a').click(function () {
         // applying again smooth scroll on menu items click
         $('html').css("scrollBehavior", "smooth");
     });
 
     // toggle menu/navbar script
-    $('.menu-btn').click(function(){
+    $('.menu-btn').click(function () {
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
 
+    // Toggle More icon on hover
+    $(".menu-btn").hover(
+        function () {
+            // Mouse enter
+            $(this).find(".fa-angle-down").hide();
+            $(this).find(".fa-angle-left").show();
+        },
+        function () {
+            // Mouse leave
+            $(this).find(".fa-angle-down").show();
+            $(this).find(".fa-angle-left").hide();
+        }
+    );
+
+    // Reset the icon when the mouse leaves the "More" link
+    $('.menu-btn').mouseleave(function () {
+        const angleDownIcon = $(this).find(".fa-angle-down");
+        const angleLeftIcon = $(this).find(".fa-angle-left");
+
+        angleDownIcon.show();
+        angleLeftIcon.hide();
+    });
+
+
+    // Reset the icon when the mouse leaves the "More" link
+    document.querySelector(".menu-btn").onmouseleave = function () {
+        const angleDownIcon = document.querySelector(".menu-btn .fa-angle-down");
+        const angleLeftIcon = document.querySelector(".menu-btn .fa-angle-left");
+
+        angleDownIcon.style.display = "inline";
+        angleLeftIcon.style.display = "none";
+    };
+
+
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Programmer", "Developer", "Graphics Designer", "Data Analyst","Freelancer"],
+        strings: ["Programmer", "Developer", "Graphics Designer", "Data Analyst", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
@@ -56,15 +90,15 @@ $(document).ready(function(){
         autoplayTimeOut: 2000,
         autoplayHoverPause: true,
         responsive: {
-            0:{
+            0: {
                 items: 1,
                 nav: false
             },
-            600:{
+            600: {
                 items: 2,
                 nav: false
             },
-            1000:{
+            1000: {
                 items: 3,
                 nav: false
             }
